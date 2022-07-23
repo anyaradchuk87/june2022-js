d324dee298c81da3a289a484d258c6be10da2783
anyaradchuk87 <anyaradchuk@gmail.com>
2022-07-23 22:11:33 +0100

    Initial commit

---
+++ HW-1/lesson1.js
@@ -0,0 +1,52 @@
+let hello, owu, com, ua, num1, num2, num3, num4, num5, num6, num7, bool1, bool2;
+hello = 'hello';
+owu = 'owu';
+com = 'com';
+ua = 'ua';
+num1 = 1;
+num2 = 10;
+num3 = -999;
+num4 = 123;
+num5 = 3.14;
+num6 = 2.7;
+num7 = 16;
+bool1 = true;
+bool2 = false;
+
+
+document.write(hello);
+document.write(owu);
+document.write(com);
+document.write(ua);
+document.write(num1);
+document.write(num2);
+document.write(num3);
+document.write(num4);
+document.write(num5);
+document.write(num6);
+document.write(num7);
+
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+
+alert(hello);
+alert(owu);
+alert(com);
+alert(ua);
+alert(num1);
+alert(num2);
+alert(num3);
+alert(num4);
+alert(num5);
+alert(num6);
+alert(num7);
